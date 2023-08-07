f3ebf71e79e320c3e01bec5a8261756afd41ddb7
Kris-js <devilmiss@mail.ru>
2023-08-08 00:42:18 +0300

    добавляет абонементы и разметку аккордеона

--- source/js/main.js
+++ source/js/main.js
@@ -3,6 +3,7 @@ import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
 import {initTabs} from './init-tabs';
 import './modules/trainer-swiper';
+import {initAccordions} from './init-accordion';
 
 // ---------------------------------
 
@@ -23,6 +24,7 @@ window.addEventListener('DOMContentLoaded', () => {
     window.form = form;
     form.init();
     initTabs();
+    initAccordions();
   });
 
 });
