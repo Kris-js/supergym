82804cc907850a5b1f976fcadc69ab7990b3e8f3
Kris-js <devilmiss@mail.ru>
2023-08-10 23:57:21 +0300

    добавляет форму и контакты

--- source/js/modules/review-swiper.js
+++ source/js/modules/review-swiper.js
@@ -9,24 +9,6 @@ const swipers = new Swiper('.review__swiper', {
     prevEl: '.review__swiper-button--prev',
     disabledClass: '.review__swiper-button--disabled',
   },
-
-  breakpoints: {
-    320: {
-      slidesPerView: 1,
-    },
-    768: {
-      slidesPerView: 2,
-      spaceBetween: 30,
-    },
-    1200: {
-      slidesPerView: 4,
-      spaceBetween: 5,
-    },
-    1360: {
-      slidesPerView: 4,
-      spaceBetween: 40,
-    },
-  },
 });
 
 export {swipers};
